11317791e723f0ec55d4ef02744d8299d2d0e9f0
jjl1203 <jjl743726293@qq.com>
2019-12-07 08:30:26 +0800

    bootstrap

--- src/main/java/top/xrk/fn436/community/CommunityApplication.java
+++ src/main/java/top/xrk/fn436/community/CommunityApplication.java
@@ -3,6 +3,9 @@ package top.xrk.fn436.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class CommunityApplication {
 
